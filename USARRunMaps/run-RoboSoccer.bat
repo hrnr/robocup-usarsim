320908caeff2f27ccce3fcea23a6138e186f082c
Arnoud Visser <A.Visser@uva.nl>
2012-03-21 18:21:14 +0100

    Added RoboSoccer map

---
+++ USARRunMaps/run-RoboSoccer.bat
@@ -0,0 +1,10 @@
+@echo on 
+
+  C:\Games\UDK\UDK-2012-02\Binaries\Win64\UDK.exe " RoboSoccer?game=USARBotAPI.BotDeathMatch -log -d3d11"
+@echo off
+REM C:\Games\UDK\UDK-2011-12\Binaries\Win32\UDK.exe " RoboSoccer?game=USARBotAPI.BotDeathMatch -log -d3d11"
+REM C:\Games\UDK\UDK-2011-10\Binaries\Win64\UDK.exe " RoboSoccer?game=USARBotAPI.BotDeathMatch -log -SM4"
+REM C:\Games\UDK\UDK-junebeta\Binaries\Win64\UDK.exe " RoboSoccer?game=USARBotAPI.ImageServerGame -log -d3d10"
+  
+@echo off  
+REM C:\Games\UDK\UDK-2011-10\Binaries\Win64\UDK.exe " RoboSoccer?game=USARBotAPI.BotDeathMatch -log -d3d11"
