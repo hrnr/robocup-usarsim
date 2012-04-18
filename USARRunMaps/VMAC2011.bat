cf8344a3cddc616a8d51c561f1488d766fde5aea
Arnoud Visser <A.Visser@uva.nl>
2012-04-18 10:29:53 +0200

    Iran Open 2012 maps

--- USARRunMaps/VMAC2011.bat
+++ USARRunMaps/VMAC2011.bat
@@ -1,1 +1,2 @@
-..\Binaries\udkLift VMAC2011?game=USARBotAPI.BotDeathMatch -log 
+ ..\Binaries\Win64\UDK.exe " VMAC2011?game=USARBotAPI.BotDeathMatch -log -d3d11"
+ rem ..\Binaries\udkLift VMAC2011?game=USARBotAPI.BotDeathMatch -log 
