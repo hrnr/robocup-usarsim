51575a5bf8a836f73e60e98cf0165d5ddb08f38b
Arnoud Visser <A.Visser@uva.nl>
2012-05-03 18:12:32 +0200

    Maps of RoboCup Dutch Open 2012

---
+++ USARRunMaps/DutchOpen2012-Final.bat
@@ -0,0 +1,3 @@
+@echo off
+
+..\Binaries\Win64\UDK DutchOpen2012-Final?game=USARBotAPI.BotDeathMatch -resX=960 -resY=720 -log -d3d11
---
+++ USARRunMaps/DutchOpen2012-SemiFinal.bat
@@ -0,0 +1,3 @@
+@echo off
+
+..\Binaries\Win64\UDK DutchOpen2012-SemiFinal?game=USARBotAPI.BotDeathMatch -resX=960 -resY=720 -log -d3d11
