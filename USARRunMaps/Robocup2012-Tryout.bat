eeb82b61efd4d5ec5277dc849fea833d36a54af0
Arnoud Visser <physar@gmail.com>
2012-06-20 02:13:44 +0200

    Tryout map of RoboCup2012, based on IranOpen2012-semi and DutchOpen-Preliminary (with modified start positions).

---
+++ USARRunMaps/Robocup2012-Tryout.bat
@@ -0,0 +1,9 @@
+:: Arnoud Visser
+:: Based by IranOpen2012-semifinal
+:: Difficulty: hard
+:: #Robots: 7 robots
+:: #Victims: 7 Victims (should study add victims button)
+
+@echo off
+
+..\Binaries\win32\udk RoboCup2012-Tryout?game=USARBotAPI.BotDeathMatch -d3d11 -log -ResX=640 -ResY=480
