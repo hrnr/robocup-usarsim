062eb08441888cad70630eff26f820ce20afb8d9
Arnoud Visser <physar@gmail.com>
2012-06-20 22:29:29 +0200

    Maps of first day of RoboCup 2012 in Mexico.

---
+++ USARRunMaps/Robocup2012-Preliminary1.bat
@@ -0,0 +1,12 @@
+:: Amir.H.Abdi
+:: Designed by UDK-2012-03 editor
+:: Difficulty: Easy
+:: #Robots: 4 robots
+:: #Victims: 4 Victims (distributed equall around the map)
+:: no fog
+:: one AirRobot allowed (Airrobot can access 3 of the victims and have visual on the 4th one.
+:: Robot Tags: Robot1,Robot2,Robot3,Robot4
+:: Comstation Tag: ComStation
+@echo off
+
+..\Binaries\win32\udk Robocup2012-Preliminary1?game=USARBotAPI.BotDeathMatch -d3d11 -log -ResX=960 -ResY=720
