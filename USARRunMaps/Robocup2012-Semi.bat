e49be660576111fe17e3e69290885d94bbcfeba3
Arnoud Visser <physar@gmail.com>
2012-07-02 15:24:51 +0200

    The maps used in the Finals of Mexico

---
+++ USARRunMaps/Robocup2012-Semi.bat
@@ -0,0 +1,12 @@
+:: Designed by UDK-2012-03 edidor
+:: Difficulty: Easy-Medium (All the teams are expected to at least score 3 victims)
+:: #Robots: 8 robots
+:: #Victims: 8 Victims (distributed equall around the map)
+:: no fog
+:: no AirRobot allowed (not designed for AirRobots)
+:: Contains two Static-Local-Smokes (rebuild the map in editor if you ran into problems)
+:: Robot Tags: Robot1,Robot2,Robot3,Robot4,Robot5,Robot6,Robot7,Robot8
+:: Comstation Tag: ComStation
+@echo off
+
+..\Binaries\win32\udk RoboCup2012-SemiFinal?game=USARBotAPI.BotDeathMatch -log 
