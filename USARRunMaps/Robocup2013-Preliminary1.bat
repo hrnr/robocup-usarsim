4ea369cf5134b6190918c4afdbd4c99e0964a940
amir_abdi <amir.h.abdi@gmail.com>
2013-06-28 18:38:47 +0200

    Maps of Preliminary2013 added.

---
+++ USARRunMaps/Robocup2013-Preliminary1.bat
@@ -0,0 +1,12 @@
+:: Designed by Sanaz Taleghani, TC member of Virtual Robot Rescue Simulation
+:: UDK-2013-02 edidor
+:: Difficulty: Easy
+:: #Robots: 4 robots
+:: #Victims: 4 Victims (distributed equall around the map)
+:: no AirRobot allowed (not designed for AirRobots) 
+:: Robot Tags: Robot1,Robot2,Robot3,Robot4
+:: Comstation Tag: ComStation
+:: Robot Type: P3AT
+@echo off
+
+..\Binaries\Win32\UDK Robocup2013-Preliminary1?game=USARBotAPI.BotDeathMatch -log 
---
+++ USARRunMaps/Robocup2013-Preliminary2.bat
@@ -0,0 +1,11 @@
+:: Designed by Amir.H.Abdi, TC member of Virtual Robot Rescue Simulation
+:: UDK-2013-02 edidor
+:: Difficulty: Medium
+:: #Robots: 4 robots
+:: #Victims: 4 Victims
+:: Robot Tags: Robot1,Robot2,Robot3,Robot4
+:: Comstation Tag: ComStation
+:: Robot Type: P3AT, AirRobot
+@echo off
+
+..\Binaries\Win32\UDK Robocup2013-Preliminary2?game=USARBotAPI.BotDeathMatch -log 
